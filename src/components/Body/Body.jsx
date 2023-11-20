f42878817ae77909430e6471221414ed1411950f
haripadhi08 <haripadhi08@gmail.com>
2023-11-20 23:42:38 +0530

    first commit

---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Body() {
+  return (
+    <>
+      <p className="para1">
+        We exist because we want to help you navigate and harness the potential
+        of this ever-changing digital landscape.
+      </p>
+      <p className="para2">
+        It's not a game of guesses,it's a game of numbers where growth is the
+        ultimate goal.We leverage cutting-edge technology and data-driven
+        insights to help you answer questions that unlock growth
+      </p>
+    </>
+  );
+}
+
+export default Body;
