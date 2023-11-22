3f9a6ca15504a95932882fc94dee70f81493a8c4
Harihar Padhi <39830449+1515236544hit@users.noreply.github.com>
2023-11-22 07:55:51 +0530

    second commit

--- src/components/Body/Body3.jsx
+++ src/components/Body/Body3.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import "./Body3.css";
+import rightarrow from "../Assets/right-arrow.png";
+import Collaborations from "../Assets/collaborations.jpg";
 
 function Body3() {
+  const imageSources = [Collaborations, Collaborations, Collaborations];
+
   return (
     <>
       <section className="sect">
         <div>
           <span className="text12">Keep Track</span>
           <span className="text21">of the wolves</span>
+          <img src={rightarrow} alt="arrow" className="arrow" />
         </div>
-        <div className="images"></div>
+        <div className="images">
+          <div className="images-row">
+            {imageSources.slice(0, 3).map((image, index) => (
+              <img
+                key={index}
+                src={image}
+                alt={`Image1 ${index + 1}`}
+                className="gallery-image"
+              />
+            ))}
+          </div>
+        </div>
       </section>
     </>
   );
